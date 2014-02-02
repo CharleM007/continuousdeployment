87a2a068663c6daef4bf26f172aafef251a01595
Philipp Garbe <pgarbe@autoscout24.com>
2014-02-03 00:16:02 +0100

    v1.0

---
+++ ContinuousDeploymentSample/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ContinuousDeploymentSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page v1.0";
+
+            return View();
+        }
+    }
+}
